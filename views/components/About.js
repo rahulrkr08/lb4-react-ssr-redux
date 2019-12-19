0984e9f2a788af3bc0cb9e5397224fc7d4fe4f8b
Rahul Radhakrishnan <rahul.radhakrishnan@kas-services.com>
2019-12-19 12:07:13 +0530

    Client events added

--- views/components/About.js
+++ views/components/About.js
@@ -4,15 +4,25 @@ import { fetchGreetingsData } from "../store";
 
 class About extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.buttonClick = this.buttonClick.bind(this);
+    }
+
     componentDidMount() {
         this.props.fetchGreetingsData();
+    }
+
+    buttonClick() {
+        alert('button click')
     }
 
     render() {
         const { greetings } = this.props;
         return (
             <div>
-                <h2>About {greetings}</h2>                
+                <h2>About {greetings}</h2> 
+                <button onClick={this.buttonClick}>Click here</button>               
             </div>
        );
     }
